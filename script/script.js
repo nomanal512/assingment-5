e112160d1ef7231523fa0ef7b3c3e20ab47414d4
nomanal512 <nomanal512@gmail.com>
2025-04-10 22:19:21 +0600

    added

--- script/script.js
+++ script/script.js
@@ -4,25 +4,27 @@ const pendingCount = document.getElementById("pendingCount");
 
 
 function handleComplete(event) {
-    
+
+    const button = event.target;
+    button.disabled = true;
+    button.style.backgroundColor = 'white';
+    button.style.color = 'black';  
+
     alert("Great job! Keep completing your tasks.");
 
-    
     let completed = parseInt(completedCount.innerText);
     let pending = parseInt(pendingCount.innerText);
 
-    
     if (pending > 0) {
         completedCount.innerText = completed - 1;
         pendingCount.innerText = pending + 1;
-        
-      
     } else {
         alert("No more pending tasks!");
     }
 }
 
-// Add event listener to each button
+
+
 completeButtons.forEach(button => {
     button.addEventListener("click", handleComplete);
 });
